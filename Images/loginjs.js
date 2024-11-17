dad304a87697195d6a3e1c0317202a6dbb67e903
Utsavdotcom <poudelutsav65@gmail.com>
2024-11-16 23:29:59 -0600

    Update loginjs.js

--- Images/loginjs.js
+++ Images/loginjs.js
@@ -5,11 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     tabs.forEach(tab => {
         tab.addEventListener('click', () => {
-            // Remove active class from all tabs and forms
             tabs.forEach(t => t.classList.remove('active'));
             forms.forEach(f => f.classList.remove('active'));
 
-            // Add active class to clicked tab and associated form
             tab.classList.add('active');
             document.getElementById(tab.dataset.tab).classList.add('active');
         });
