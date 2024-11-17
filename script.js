827749911da684c7225a16138de78b75d31a5ed1
Utsavdotcom <poudelutsav65@gmail.com>
2024-11-16 23:27:35 -0600

    updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const text = "Plexi is under development, thank you for visiting us. We are on our way to make Plexi even better than ever. Hope to see you soon.";
-    const typingSpeed = 50; // Adjust typing speed (milliseconds per character)
+    const typingSpeed = 50;
     const element = document.getElementById('loading-message');
     
     let index = 0;
@@ -11,10 +11,47 @@ document.addEventListener("DOMContentLoaded", function() {
             index++;
             setTimeout(type, typingSpeed);
         } else {
-            // Optional: Blink caret after typing is complete
             element.classList.add('blinking-caret');
         }
     }
     
     type();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle('active');
+        menu.classList.toggle('show');
+    });
+});
+
+
+document.querySelectorAll('.has-dropdown > a').forEach((menuItem) => {
+    menuItem.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const dropdown = menuItem.nextElementSibling;
+
+
+        dropdown.classList.toggle('show');
+
+        document.querySelectorAll('.dropdown').forEach((otherDropdown) => {
+            if (otherDropdown !== dropdown) {
+                otherDropdown.classList.remove('show');
+            }
+        });
+    });
+});
+
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('.has-dropdown')) {
+        document.querySelectorAll('.dropdown').forEach((dropdown) => {
+            dropdown.classList.remove('show');
+        });
+    }
+});
+
+
